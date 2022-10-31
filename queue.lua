e866ad0aecddbd151628a9e51f7f4de485484690
Ethan <61071128+Ethanoj1@users.noreply.github.com>
2022-10-31 12:42:22 -0700

    Update queue.lua

--- queue.lua
+++ queue.lua
@@ -4,4 +4,5 @@ getgenv().mainKey = "%s";
 getgenv()._flags = '%s';
 warn(mainKey, getgenv().mainKey);
 
-local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https\58//api.eclipsehub.xyz/auth";c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
+local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https\58//api.eclipsehub.xyz/auth";c(a and b,"Executor not Supported")a(b({Url=e.."?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
+warn("Loaded ls")
