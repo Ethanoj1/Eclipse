ed5814d8f02905612b15ed3ce82c0dc4f6a9eaa6
Ethan <61071128+Ethanoj1@users.noreply.github.com>
2025-10-25 22:49:00 -0700

    Update queue2.lua

--- queue2.lua
+++ queue2.lua
@@ -7,4 +7,4 @@ task.wait(5);
 getgenv().mainKey = "%s";
 getgenv()._flags = '%s';
 
-local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https\58//api.eclipsehub.xyz/auth";c(a and b,"Executor not Supported")a(b({Url=e.."?\107e\121\61"..d(mainKey).."\38\116\61%d",Headers={["User-Agent"]="Eclipse"}}).Body)()
+local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https\58//api.eclipsehub.xyz/auth";c(a and b,"Executor not Supported")a(b({Url=e.."?\107e\121\61"..d(mainKey).."\38\116=%d",Headers={["User-Agent"]="Eclipse"}}).Body)()
